eac33960ee7fb13d5f8a1777c688fc3d16a5938e
Leo <axesan917@gmail.com>
2025-04-26 14:08:02 -0600

    I haved to configure mi crontab for automatized

---
+++ backup_home2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Variables
+BACKUP_DIR="$HOME/backups"
+DATE=$(date +%Y-%m-%d_%H-%M-%S)
+BACKUP_FILE="$BACKUP_DIR_/backup_$DATE.tar.gz"
+SOURCE_DIR="$HOME"
+DAYS_TO_KEEP=7 # <-- Numero de dias que quieres conservar los backups
+
+# Crear carpeta de backups si no existe
+mkdir -p "$BACKUP_DIR"
+
+# Crear el backup 
+tar -czvf "$BACKUP_FILE" "$SOURCE_DIR"
+
+# Borrar backups mas viejos que $DAYS_TO_KEEP dias
+find "$BACKUP_DIR" -type f -name "backup_*.tar.gz" -mtime +$DAYS_TO_KEEP -exec rm {} \;
+
+# Mensaje Final 
+
+echo "Backup creado: $BACKUP_FILE"
+echo "Backups mas antiguos de $DAYS_TO_KEEP dias han sido eliminados"
